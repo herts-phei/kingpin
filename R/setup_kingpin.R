ab44e47525995a13fa2a5bc8a7cadc19e5ce3aeb
ayu <Annie.Yu@hertfordshire.gov.uk>
2022-10-14 11:06:49 +0000

    automatically adjust to default permissions for Epi team

--- R/setup_kingpin.R
+++ R/setup_kingpin.R
@@ -17,7 +17,10 @@
 #' # Basic usage, assuming .Renviron is set up with CONNECT_SERVER and CONNECT_API_SERVER environmental variables:
 #' setup_kingpin(server = Sys.getenv("CONNECT_SERVER"), key = Sys.getenv("CONNECT_API_KEY"))
 #'
-setup_kingpin <- function(server, key, force = FALSE) {
+setup_kingpin <- function(server,
+                          key,
+                          force = FALSE,
+                          group = "Epi") {
 
   board <- pins::board_rsconnect(server = server,
                                  key = key)
@@ -56,7 +59,38 @@ setup_kingpin <- function(server, key, force = FALSE) {
     }
 
   } else {
+
+    # PIN KINGPIN
     pins::pin_write(board, kingpin, "kingpin")
+    message("Kingpin on board.")
+
+    # ADJUST PERMISSIONS
+    call_group <- httr::GET(paste0(server, "__api__/v1/groups"),
+                        httr::add_headers(Authorization = paste("Key", key)))
+
+    guid <- dplyr::bind_rows(httr::content(call_group)$results) |>
+      dplyr::filter(name == group) |>
+      dplyr::pull(guid)
+
+    call_pins <- httr::GET(paste0(server, "__api__/v1/content"),
+                           httr::add_headers(Authorization = paste("Key", key)))
+
+    id <- dplyr::bind_rows(httr::content(call_pins)) |>
+      dplyr::filter(name == "kingpin") |>
+      dplyr::pull(guid)
+
+    body <- paste0('{
+    "principal_guid": "', guid, '",
+    "principal_type": "group",
+    "role": "owner"
+    }')
+
+    result <- httr::POST(paste0(server, "__api__/v1/content/", id, "/permissions"),
+                   body = body, encode = "raw",
+                   httr::add_headers(Authorization = paste("Key", key)))
+
+    message(paste0("Owner permissions given to group ", group))
+
   }
 
 }
